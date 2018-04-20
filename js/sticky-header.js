0afe20a3566b400388e2a5738c46dc7bb364f103
Elena <harmony3003@gmail.com>
2018-04-20 10:04:25 +0300

    Подключение js прилипание меню

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,17 +1,17 @@
 (function() {
   
-  var pageHeader = document.querySelector(".page_header");
+  var pageHeader = document.querySelector(".nav_bar");
   var pageHeaderHeight = pageHeader.getBoundingClientRect().bottom + window.pageYOffset;
   
   window.addEventListener("scroll", function() {
     
-    if (pageHeader.classList.contains("page_header-sticky") && (window.pageYOffset < pageHeaderHeight)) {
+    if (pageHeader.classList.contains("nav_bar-sticky") && (window.pageYOffset < pageHeaderHeight)) {
         
-      pageHeader.classList.remove("page_header-sticky");
+      pageHeader.classList.remove("nav_bar-sticky");
         
     } else if (window.pageYOffset > pageHeaderHeight) {
         
-      pageHeader.classList.add("page_header-sticky");
+      pageHeader.classList.add("nav_bar-sticky");
         
     }
     
